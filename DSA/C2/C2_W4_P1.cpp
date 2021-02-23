5618a38be967c7c90de07aefebbf4bc36a75b3ee
Elessurgon <russell10mathias@gmail.com>
2021-02-23 21:22:37 +0530

    week 4 AC

---
+++ DSA/C2/C2_W4_P1.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, a, b) for (ll i = a; i <= b; i++)
+#define REP_REV(i, a, b) for (ll i = a; i >= b; i--)
+#define debug(x) cerr << #x << " is " << x << endl;
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+#define pb push_back
+#define rsz resize
+#define f first
+#define s second
+#define mp make_pair
+
+using ll = int64_t;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vc = vector<char>;
+using ull = unsigned int64_t;
+using pi = pair<int, int>;
+using vl = vector<int64_t>;
+using pl = pair<int64_t, int64_t>;
+using vpl = vector<pair<int64_t, int64_t>>;
+using msl = map<string, int64_t>;
+using vvl = vector<vector<int64_t>>;
+
+const ll MOD = 1e9 + 7;
+const int INF = int(1e9);
+
+bool isPrime(ll n) {
+    if (n == 2) return true;
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+unsigned long long factorial(int n) {
+    const unsigned int M = MOD;
+    unsigned long long f = 1;
+    for (int i = 1; i <= n; i++)
+        f = (f * i) % M;
+    return f;
+}
+
+ll expon(ll a, ll b) {
+    ll ans = 1;
+    REP(i, 1, b) {
+        ans *= a;
+    }
+    return ans;
+}
+
+ll n;
+vl t, l, r;
+
+void preorder(ll index) {
+    if (index != -1) preorder(l[index]);
+    if (index != -1) cout << t[index] << " ";
+    if (index != -1) preorder(r[index]);
+}
+
+void inorder(ll index) {
+    if (index != -1) cout << t[index] << " ";
+    if (index != -1) inorder(l[index]);
+    if (index != -1) inorder(r[index]);
+}
+
+void postorder(ll index) {
+    if (index != -1) postorder(l[index]);
+    if (index != -1) postorder(r[index]);
+    if (index != -1) cout << t[index] << " ";
+}
+
+void solve() {
+    cin >> n;
+
+    REP(i, 0, n - 1) {
+        ll T, L, R;
+        cin >> T >> L >> R;
+        t.pb(T);
+        l.pb(L);
+        r.pb(R);
+    }
+
+    preorder(0);
+    cout << endl;
+    inorder(0);
+    cout << endl;
+    postorder(0);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // freopen("censor.in", "r", stdin);
+    // freopen("censor.out", "w", stdout);
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
+    return 0;
+}
