bc690c2595791d818d134f6e4f48d537c1c45dab
Elessurgon <LancelotSycorax@gmail.com>
2021-10-04 22:47:09 +0530

    .

--- CP.cpp
+++ CP.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 #define REP(i, a, b) for (ll i = a; i <= b; i++)
 #define REP_REV(i, a, b) for (ll i = a; i >= b; i--)
-#define debug(x) cerr << #x << " is " << x << endl;
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
@@ -21,6 +20,7 @@ using namespace std;
 #define mp make_pair
 
 using ll = int64_t;
+using vb = vector<bool>;
 using vi = vector<int>;
 using vvi = vector<vector<int>>;
 using vc = vector<char>;
@@ -32,10 +32,36 @@ using vpl = vector<pair<int64_t, int64_t>>;
 using msl = map<string, int64_t>;
 using vvl = vector<vector<int64_t>>;
 
-#define DBG1(x) cerr << (#x) << ": " << (x) << endl;
-#define DBG2(x, y) cerr << (#x) << " " << (#y) << ": " << (x) << " " << y << endl;
-#define DBG3(x, y, z) cerr << (#x) << " " << (#y) << " " << (#z) << ": " << (x) << " " << y << " " << z << endl;
-#define DBG4(a, b, c, d) cerr << (#a) << " " << (#b) << " " << (#c) << " " << (#d) << ": " << a << " " << b << " " << c << " " << d << endl;
+const ll MOD = 1e9 + 7;
+const ll INF = numeric_limits<ll>::max();
+
+vector<string> vec_splitter(string s) {
+    s += ',';
+    vector<string> res;
+    while (!s.empty()) {
+        res.push_back(s.substr(0, s.find(',')));
+        s = s.substr(s.find(',') + 1);
+    }
+    return res;
+}
+void debug_out(
+    vector<string> __attribute__((unused)) args,
+    __attribute__((unused)) int idx,
+    __attribute__((unused)) int LINE_NUM) { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+    if (idx > 0)
+        cerr << ", ";
+    else
+        cerr << "Line(" << LINE_NUM << ") ";
+    stringstream ss;
+    ss << H;
+    cerr << args[idx] << " = " << ss.str();
+    debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#define DBG(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+
 template <class T>
 void DBGvec(vector<T> a) {
     for (T i : a) cerr << i << " ";
@@ -51,9 +77,6 @@ void DBGmap(map<K, P> a) {
     for (auto i : a) cerr << "{" << i.f << " " << i.s << "} ";
     cerr << endl;
 }
-
-const ll MOD = 1e9 + 7;
-const int INF = int(1e18);
 
 bool isPrime(ll n) {
     if (n == 1) return false;
@@ -101,10 +124,10 @@ int main() {
     // freopen("paintbarn.in", "r", stdin);
     // freopen("paintbarn.out", "w", stdout);
 
-    ll t;
-    cin >> t;
-    while (t--)
-        solve();
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
 
 #ifdef _DEBUG
     cerr << "\nTIME = " << clock() - tt << endl;
