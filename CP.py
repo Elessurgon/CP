668cb27ada9342fa7a45cc48b7ed550e123de3dc
Elessurgon <LancelotSycorax@gmail.com>
2021-10-04 23:32:46 +0530

    .

--- CP.py
+++ CP.py
@@ -20,54 +20,11 @@ def invr():
     return(map(int, input().split()))
 
 
-def main():
+def solve():
     pass
 
-
-S = input()
-
-
-def check(mid, array, n, K):
-    count = 0
-    sum = 0
-    for i in range(n):
-        if (array[i] > mid):
-            return False
-        sum += array[i]
-        if (sum > mid):
-            count += 1
-            sum = array[i]
-    count += 1
-    if (count <= K):
-        return True
-    return False
-
-
-def solve(array, n, K):
-    start = max(array)
-    end = 0
-    for i in range(n):
-        end += array[i]
-    answer = 0
-    while (start <= end):
-        mid = (start + end) // 2
-        if (check(mid, array, n, K)):
-            answer = mid
-            end = mid - 1
-        else:
-            start = mid + 1
-    return answer
-
-
-x = inlt()
-print(x)
-a = x[0]
-b = x[1]
-c = x[2]
-
-if pow(a, b) < pow(b, c):
-    print("<")
-elif pow(a, b) > pow(b, c):
-    print(">")
-else:
-    print("=")
+solve()
+# t = inp()
+# while(t):
+#     solve()
+#     t -= 1
