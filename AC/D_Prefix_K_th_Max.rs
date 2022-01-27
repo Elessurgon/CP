19e170fd7cd16064e5d95fde071ac7672d154cd0
Elessurgon <LancelotSycorax@gmail.com>
2022-01-27 11:32:11 +0530

    min priority queue mess

---
+++ AC/D_Prefix_K_th_Max.rs
@@ -0,0 +1,66 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused)]
+
+use std::cmp::{max, min, Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::io::{self, prelude::*};
+use std::io::{stdin, stdout, BufWriter, Write};
+use std::str;
+const BITS: usize = 19;
+const INF: i64 = 0x3f3f3f3f;
+
+#[derive(Default)]
+
+struct Scanner {
+    buffer: Vec<String>,
+}
+impl Scanner {
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buffer.pop() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Failed read");
+            self.buffer = input.split_whitespace().rev().map(String::from).collect();
+        }
+    }
+}
+
+fn solve() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+    let n: i32 = scan.next();
+    let k: i32 = scan.next();
+    let mut heap = BinaryHeap::new();
+    for i in 0..k {
+        let ele: i32 = scan.next();
+        heap.push(-ele);
+    }
+    let num = heap.peek().unwrap();
+    writeln!(out, "{}", -num);
+    for i in k..n {
+        let ele: i32 = scan.next();
+
+        if let Some(top) = heap.peek() {
+            if -top <= ele {
+                heap.push(-ele);
+                heap.pop();
+            }
+        }
+        let num = heap.peek().unwrap();
+        writeln!(out, "{}", -num);
+    }
+}
+
+fn main() {
+    let mut scan = Scanner::default();
+    let out = &mut BufWriter::new(stdout());
+
+    // let t: i64 = scan.next();
+    // for _ in 0..t {
+    // solve();
+    // }
+    solve();
+}
